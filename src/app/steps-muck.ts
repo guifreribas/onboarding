5e386de5e0a4fa88ed26857e13c68e15510cf7d3
Guifré Puig Ribas <gpuig@brothapps.com>
2024-05-10 15:06:57 +0200

    Exercici 3

--- src/app/steps-muck.ts
+++ src/app/steps-muck.ts
@@ -5,21 +5,21 @@ export const steps: IStep[] = [
     title: 'Dedica moltes hores',
     descripcion:
       'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
-    img: '',
-    bgcolor: 'blue',
+    img: 'assets/time_managment.svg',
+    bgcolor: 'green',
   },
   {
     title: 'Programa projectes propis',
     descripcion:
       'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
-    img: '',
-    bgcolor: 'red',
+    img: 'assets/programming.svg',
+    bgcolor: 'gray',
   },
   {
     title: 'Procura descansar',
     descripcion:
       "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
-    img: '',
-    bgcolor: 'green',
+    img: 'assets/meditation.svg',
+    bgcolor: 'yellow',
   },
 ];
