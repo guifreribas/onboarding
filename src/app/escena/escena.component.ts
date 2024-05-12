c39f9d67f27c05a93cbfe3087e37c747fc2766fe
Guifré Puig Ribas <gpuig@brothapps.com>
2024-05-12 18:49:59 +0200

    Exercici 4

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,15 +1,27 @@
 import { Component, Input } from '@angular/core';
 import { IStep } from '../models/i-step';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-escena',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './escena.component.html',
   styleUrl: './escena.component.scss',
 })
 export class EscenaComponent {
   @Input() steps!: IStep[];
+  currentStep: number = 0;
+
+  goNextStep(): void {
+    this.currentStep++;
+    console.log('goNextStep', this.currentStep);
+  }
+
+  goPrevStep(): void {
+    this.currentStep--;
+    console.log('goPrevStep', this.currentStep);
+  }
 
   ngOnInit(): void {
     console.log(this.steps);
