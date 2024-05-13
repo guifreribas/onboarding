e2f26e77f1f8f8695466ff20cf3b7d1148c485ec
Guifré Puig Ribas <gpuig@brothapps.com>
2024-05-13 09:55:53 +0200

    Exercici 6

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,6 +1,91 @@
 import { Component, Input } from '@angular/core';
 import { IStep } from '../models/i-step';
 import { CommonModule } from '@angular/common';
+import {
+  trigger,
+  transition,
+  query,
+  style,
+  animate,
+  group,
+} from '@angular/animations';
+
+const left = [
+  query(
+    ':enter, :leave',
+    style({ position: 'fixed', width: '100%', opacity: 1, scale: 1 }),
+    {
+      optional: true,
+    }
+  ),
+  group([
+    query(
+      ':enter',
+      [
+        style({ transform: 'translateX(-100%)', opacity: 0, scale: 1.3 }),
+        animate(
+          '.3s ease-out',
+          style({ transform: 'translateX(0%)', opacity: 1, scale: 1 })
+        ),
+      ],
+
+      {
+        optional: true,
+      }
+    ),
+    query(
+      ':leave',
+      [
+        style({ transform: 'translateX(0%)', opacity: 1 }),
+        animate(
+          '.3s ease-out',
+          style({ transform: 'translateX(100%)', opacity: 0, scale: 1.3 })
+        ),
+      ],
+      {
+        optional: true,
+      }
+    ),
+  ]),
+];
+
+const right = [
+  query(
+    ':enter, :leave',
+    style({ position: 'fixed', width: '100%', opacity: 1, scale: 1 }),
+    {
+      optional: true,
+    }
+  ),
+  group([
+    query(
+      ':enter',
+      [
+        style({ transform: 'translateX(100%)', opacity: 0, scale: 1.3 }),
+        animate(
+          '.3s ease-out',
+          style({ transform: 'translateX(0%)', opacity: 1, scale: 1 })
+        ),
+      ],
+      {
+        optional: true,
+      }
+    ),
+    query(
+      ':leave',
+      [
+        style({ transform: 'translateX(0%)', opacity: 1, scale: 1 }),
+        animate(
+          '.3s ease-out',
+          style({ transform: 'translateX(-100%)', opacity: 0, scale: 1.3 })
+        ),
+      ],
+      {
+        optional: true,
+      }
+    ),
+  ]),
+];
 
 @Component({
   selector: 'app-escena',
@@ -8,6 +93,12 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './escena.component.html',
   styleUrl: './escena.component.scss',
+  animations: [
+    trigger('animatedSlide', [
+      transition(':decrement', left),
+      transition(':increment', right),
+    ]),
+  ],
 })
 export class EscenaComponent {
   @Input() steps!: IStep[];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { EscenaComponent } from '../escena/escena.component';
 import { StepsService } from '../steps.service';
 import { IStep } from '../models/i-step';
+import { steps } from '../steps-muck';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +12,13 @@ import { IStep } from '../models/i-step';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  newSteps: IStep[] = [];
   constructor(private stepService: StepsService) {}
   steps: IStep[] = [];
+
+  getNewSteps(): void {
+    this.newSteps = steps;
+  }
 
   getSteps(): void {
     this.steps = this.stepService.getSetps();
